99cb9fd6a83001ae3e707cc337751c8ef2550d7d
Alfa <opportunities91@gmail.com>
2025-07-02 15:11:17 -0700

    project update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,26 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
-const { UNAUTHORIZED } = require("../utils/statusCodes");
+const UnauthorizedError = require("../utils/errors/UnauthorizedError");
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return res.status(UNAUTHORIZED).send({ message: "Authorization required" });
+    throw new UnauthorizedError("Authorization required");
   }
 
   const token = authorization.replace("Bearer ", "");
+
   try {
     const payload = jwt.verify(token, JWT_SECRET);
     req.user = payload;
-    return next();
+    next();
   } catch (err) {
-    return res.status(UNAUTHORIZED).send({ message: "Invalid token" });
+    throw new UnauthorizedError("Invalid token");
   }
 };
 
 module.exports = auth;
+
+
+
